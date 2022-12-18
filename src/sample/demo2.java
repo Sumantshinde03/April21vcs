3f178ab362765d4a3f04ac6a4c65266ec3d73eb9
User <User@DESKTOP-GVRSC5L>
2022-12-18 15:49:31 +0530

    [sumant]udate

---
+++ src/sample/demo2.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Gm");
+		System.out.println("Gm");
+		System.out.println("Gm");
+		System.out.println("Gm");
+		System.out.println("Gm");
+		
+
+	}
+
+}
